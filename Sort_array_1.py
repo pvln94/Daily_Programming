97a48de62c6bb84fe9c877acc5f49d42dd557e9d
PULA V LAKSHMI NARASIMHA NAIDU <av.en.u4aie22029@av.students.amrita.edu>
2024-09-09 10:47:19 +0530

    Sort_array_1.py

--- Sort_array_1.py
+++ Sort_array_1.py
@@ -1,9 +1,22 @@
 def S(x):
     if x:
-        x.sort()
+        l = 0
+        m = 0
+        h = len(x) - 1
+        
+        while m <= h:
+            if x[m] == 0:
+                x[l], x[m] = x[m], x[l]
+                l += 1
+                m += 1
+            elif x[m] == 1:
+                m += 1
+            else: 
+                x[h], x[m] = x[m], x[h]
+                h -= 1
         print(x)
     else:
-        print(x)  
+        print([])  
 
 if __name__ == "__main__":
     n = input()
