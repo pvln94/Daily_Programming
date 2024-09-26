ceae58f1e0a8ec95e06b3344aa18adce4fe8126c
PULA V LAKSHMI NARASIMHA NAIDU <av.en.u4aie22029@av.students.amrita.edu>
2024-09-26 18:38:44 +0530

    Count_Numberof_Divisors_18.py

---
+++ Count_Numberof_Divisors_18.py
@@ -0,0 +1,14 @@
+import math
+
+def count_divisors(N):
+    count = 0
+    for i in range(1, int(math.sqrt(N)) + 1):
+        if N % i == 0:
+            if i == N // i:
+                count += 1
+            else:
+                count += 2
+    return count
+
+N = int(input())
+print(count_divisors(N))
